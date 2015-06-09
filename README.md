29c8b52c2b8f3f195badd837d724032b9c1d1dfe
Kevin Lacker <lacker@gmail.com>
2015-06-08 21:58:25 -0700

    basic setup for isomorphic es6

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cardkit
 A bunch of tools for building card games.
+
+Set up with `npm install`
+
+`npm start` runs locally at localhost:8080/webpack-dev-server
---
+++ js/app.js
@@ -0,0 +1,12 @@
+import React from 'react';  
+
+let App = React.createClass({  
+  render() {
+    return (
+      <div className="app">
+        Hello world
+      </div>
+    );
+  }
+});
+
