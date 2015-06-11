73527e4f009af8799c51a7e8e8233766bd71738d
Kevin Lacker <lacker@gmail.com>
2015-06-10 23:43:46 -0700

    use a Connection object for better server debug info

--- js/server.js
+++ js/server.js
@@ -4,22 +4,36 @@ const WebSocketServer = require("ws").Server
 
 let wss = new WebSocketServer({port: 9090})
 
+class Connection {
+  constructor(ws) {
+    this.ws = ws
+
+    console.log(`hello ${this.clientName()}`)
+  }
+
+  clientName() {
+    return `${this.ws._socket.remoteAddress}:${this.ws._socket.remotePort}`
+  }
+
+  close() {
+    console.log(`goodbye ${this.clientName()}`)
+  }
+}
+
 wss.on("connection", function(ws) {
-  console.log(`hello ${ws.address}`)
+  let connection = new Connection(ws)
 
   ws.on("message", function(message) {
     console.log("bouncing: %s", message)
 
     for (let client of wss.clients) {
-      client.send(message);
+      client.send(message)
     }
   })
 
-  ws.on("close", function() {
-    console.log(`goodbye ${ws.address}`)
-  })
+  ws.on("close", () => connection.close())
 
-  ws.send("hello");
+  ws.send("hello")
 })
 
 console.log("server running...")
