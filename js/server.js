fca42812ad939a564423adbccbd139624b063217
Kevin Lacker <lacker@gmail.com>
2015-06-11 22:16:14 -0700

    server protocol to do matchmaking

--- js/server.js
+++ js/server.js
@@ -1,5 +1,16 @@
 // This server just broadcasts all messages to all clients.
 
+// Client protocol:
+// To sign up for a game:
+// { "op": "register", "me": "yourPlayerName"}
+
+// Server protocol:
+// The server bounces any client messages to all clients.
+// When a client connects, the server sends it
+// { "op": "hello" }
+// When a game starts, the server sends out
+// { "op": "start", "players": [...list of player names...]}
+
 const WebSocketServer = require("ws").Server
 
 let wss = new WebSocketServer({port: 9090})
@@ -7,23 +18,54 @@ let wss = new WebSocketServer({port: 9090})
 class Connection {
   constructor(ws) {
     this.ws = ws
+    this.name = null
+    console.log(`hello ${this.address()}`)
 
-    console.log(`hello ${this.clientName()}`)
-
-    // Connection.all maps client names to connected clients
     if (Connection.all === undefined) {
-      Connection.all = {}
+      // Connection.all maps client addresses to connected clients
+      Connection.all = new Map()
+
+      // Connection.waiting is a map of clients that have sent a
+      // register operation, keyed by name.
+      Connection.waiting = new Map()
     }
-    Connection.all[this.clientName()] = this
+    Connection.all.set(this.address(), this)
   }
 
-  clientName() {
+  address() {
     return `${this.ws._socket.remoteAddress}:${this.ws._socket.remotePort}`
+  }
+    
+
+  // Message should be JSON
+  broadcast(message) {
+
+  }
+
+  receive(messageData) {
+    let message = JSON.parse(messageData)
+    if (message.op == "register") {
+      this.name = message.name
+      Connection.waiting.set(this.name, this)
+    }
+
+    // Bounce it
+    this.broadcast(message)
+
+    // Consider starting a new game
+    if (Connection.waiting.size == 2) {
+      let start = { op: "start", players: Connection.waiting.keys() }
+      this.broadcast(start)
+      Connection.waiting.clear()
+    }
   }
 
   close() {
-    console.log(`goodbye ${this.clientName()}`)
-    delete Connection.all[this.clientName()]
+    console.log(`goodbye ${this.address()}`)
+    Connection.all.delete(this.address())
+    if (this.name != null && Connection.waiting.has(this.name)) {
+      Connection.waiting.delete(this.name)
+    }
   }
 }
 
@@ -31,16 +73,12 @@ wss.on("connection", function(ws) {
   let connection = new Connection(ws)
 
   ws.on("message", function(message) {
-    console.log("bouncing: %s", message)
-
-    for (let client of wss.clients) {
-      client.send(message)
-    }
+    connection.receive(message)
   })
 
   ws.on("close", () => connection.close())
 
-  ws.send("hello")
+  ws.send('{"op":"hello"}')
 })
 
 console.log("server running...")
