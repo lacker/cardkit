6b0e651220c00360aebc62d402d6380b017a6cc5
andrewljohnson <andrew@gaiagps.com>
2015-06-13 01:50:55 -0700

    comments

--- js/cardview.js
+++ js/cardview.js
@@ -21,8 +21,9 @@ let Card = React.createClass({
 
   // click cards in current player's hand or board
   clickCard: function() {
-
-    // can click opponent's in play cards i
+    
+    // ATTACK
+    // can click opponent's in play cards
     // after player has clicked his own in play card to attack
     var hasActiveCard = false;
     var fromAttackIndex = 0;
@@ -37,16 +38,11 @@ let Card = React.createClass({
     }
     var toIndex = window.game.opponent().board.indexOf(this.props.cardInfo);
     if (toIndex != -1 && hasActiveCard) {
-      var move = {"op":"attack", 
-                  "from":fromAttackIndex,
-                  "to": toIndex
-                 };
-      window.client.makeLocalMove(move);
-      window.client.gameView.forceUpdate();
-      card.hasAttacked = true;
+      this.attack(fromAttackIndex, toIndex, attackingCard)
       return;      
     }
 
+    // PLAY CARD FROM HAND
     // can click to play cards in hand
     var fromIndex = window.game.current().hand.indexOf(this.props.cardInfo);
     if (fromIndex != -1) {
@@ -54,6 +50,7 @@ let Card = React.createClass({
       return;
     }
     
+    // HIGHLIGHT CARD, OR SMASH FACE IF ALREADY HIIGHLIGHTED
     // can click a card in play if it's in the player's board
     // and it's not summoning sick or already attacked
     fromIndex = window.game.current().board.indexOf(this.props.cardInfo);
@@ -66,6 +63,17 @@ let Card = React.createClass({
 
     // if it's not in their hand or board, then do nothing
 
+  },
+  
+  // smash creature's face
+  attackCreature: function(fromAttackIndex, toIndex, attackingCard) {
+    var move = {"op":"attack", 
+                "from":fromAttackIndex,
+                "to": toIndex
+               };
+    window.client.makeLocalMove(move);
+    window.client.gameView.forceUpdate();
+    attackingCard.hasAttacked = true;
   },
 
   // highlight a card when clicked, play when double clicked
