48c1b7fbf55036a67bb9993bfdc3a54938a66d81
Andrew Johnson <andrewljohnson@gmail.com>
2015-08-14 23:21:27 -0700

    add flavor and rules text

--- src/js/views/lobbyview/lobbyview.js
+++ src/js/views/lobbyview/lobbyview.js
@@ -21,23 +21,43 @@ export default class LobbyView extends Component {
     window.client.forceUpdate();
   }
 
+  showRules() {
+    alert("* You start with 1 card, and 3 energy to play cards. \
+          \n\n* Every few seconds, you draw, and get more energy. \
+          \n\n* Each creature you play attacks every few seconds. \
+          \n\n* Creatures attack players, unless you retarget them. Click a creature and then a target to retarget. \
+          \n\n* First player to lose all life loses."
+         )
+  }
+
   render() {
     const block = this.props.name,
     elm = Util.buildElementClasses(block, [
       'heading',
-      'campaign-btn',
+      'basic-btn',
+      'float-right',
       'home-img'
     ]);
+
+    let classNames = require('classnames');
+    let classes = classNames(elm.basicBtn);
 
     return (
       <div className={block}>
         <h1 className={elm.heading}>Welcome to Spacetime</h1>
-        <button className={elm.campaignBtn} onClick={this.playCampaign}>
-          Campaign 1: Bibot Attack
+        <button className={elm.basicBtn} onClick={this.playCampaign}>
+          Campaign 1: BiBot Attack
         </button>
+        <button className={classes} onClick={this.showRules}>
+          How to Play
+        </button>
+        <p>
+          The BiBot factory has been hacked, and you must go reclaim it. <br />
+          The hacker is churning out bibots as fast as possible, all set to KILL.
+        </p>
         <img className={elm.homeImg} src={galaxyShadowImg} />
       </div>
     );
   }
-  
+
 }
